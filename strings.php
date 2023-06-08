4947cefe46c04f400a480833b82db7e5585fda1f
Franklim Porfirio <poorfiriof@gmail.com>
2023-06-07 21:33:37 -0300

    Strings
    Teste com strings, soma de valores na string e tags HTML nas strings

---
+++ strings.php
@@ -0,0 +1,35 @@
+<?php
+    //Strings
+    //Strings são uma sequencia de caracteres, geralmente utilizada para representar palavras
+    // frases ou textos de um programa.
+
+    echo "Curso de PHP"; // uma string em PHP pode ser criada com aspas duplas ou simples.
+
+    $string = "\nTeste de string \n"; // essa variavel está armazenando uma string
+    $valorDia = "80";
+    $gorjetas = "15";
+    $soma = $valorDia + $gorjetas;// é possivel somar os valores dentro de uma string
+
+    // tambem é possivel adicionar tags de HTML nas strings
+    $h1 = "<h1>Teste</h1>";
+    $h2 = "<h2>Teste</h2>";
+    $h3 = "<h3>Teste</h3>";
+    $h4 = "<h4>Teste</h4>";
+    $h5 = "<h5>Teste</h5>";
+
+
+    echo $soma; // é possivel somar os valores dentro de uma string
+
+    echo $string;
+    echo $h1;
+    echo $h2;
+    echo $h3;
+    echo $h4;
+    echo $h5;
+
+
+
+
+
+
+?>
