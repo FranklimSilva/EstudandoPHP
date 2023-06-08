ce5e81bf413370bac974a11b8aa8115f40efbcfe
Franklim Porfirio <poorfiriof@gmail.com>
2023-06-07 22:15:39 -0300

    Array
    É uma estrutura de dados que armazena uma coleção de elementos de tal forma que cada um dos elementos possa ser identificado por, pelo menos, um indice ou chave.

---
+++ array.php
@@ -0,0 +1,14 @@
+<?php 
+        //ARRAY
+        //É uma estrutura de dados que armazena uma coleção de elementos de tal forma
+        // que cada um dos elementos psssa ser identificado por, pelo menos, um indice ou chave
+
+        $cursos = array("PHP", "JAVA","C++", "C#");
+
+        print_r($cursos); // mostra o array com seus indices
+
+        print_r($cursos[2]); // mostra o elemento do array na posição de numero 2(lembrando de começa a contagem a partir de zero)
+
+
+
+?>
