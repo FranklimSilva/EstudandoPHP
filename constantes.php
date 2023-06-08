3fa3d10fe506f3a64a6a20788d983f23a527d1d3
Franklim Porfirio <poorfiriof@gmail.com>
2023-06-07 21:12:27 -0300

    Constants
    Valor que não vai mudar por outros fatores.

---
+++ constantes.php
@@ -0,0 +1,15 @@
+<?php
+//Constantes
+//É um recurso das linguagens de programação que tem como objetivo armazenar um 
+//valor em memória de modo que não possa ser alterado.
+
+define("CONSTANT", "Tipos de variáveis em PHP.\n");
+echo CONSTANT;
+
+
+    define("valorDia", "80"); // valor que não vai mudar por outros fatores.
+    define("gorjetas","40");
+
+    echo valorDia + gorjetas;
+
+?>
