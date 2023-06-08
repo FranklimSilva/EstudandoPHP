4f09bfab3630abf1f406c9ed56ce934d470cf314
Franklim Porfirio <poorfiriof@gmail.com>
2023-06-07 21:45:43 -0300

    Numeros
    Os integers ou números são todos os números negativos ou positivos. No PHP pode ser INTEGER ou LONG

---
+++ integersOrNumbers.php
@@ -0,0 +1,19 @@
+<?php
+    //NUMEROS ou INTEGERS
+        //Os integers são todos os numeros negativos ou positivos. No PHP pode ser INTEGER ou LONG
+        // LONG = Numero de bytes para armazenar variavel, armazena uma faixa maior de valores.
+        // INTEGERS = Utilizado para armazenar numeros menores.
+
+    $valorDia = 80;
+    $gorjetas = 15;
+    $combustivel = -30;
+
+    $sum = $valorDia + $gorjetas + $combustivel;
+
+    var_dump($sum);
+
+    
+
+
+
+?>
