c081b23e1b423cbc39b3deae332e277e5e7889e4
Franklim Porfirio <poorfiriof@gmail.com>
2023-06-13 19:42:36 -0300

    CondicionalIf
    Na pratica como o If funciona.

---
+++ Estruturas/conficionalIf.php
@@ -0,0 +1,14 @@
+<?php
+$variavel1 = true;
+$variavel2 = true;
+$variavel3 = true;
+
+//if é o SE, ele faz com que se uma condição for verdadeira ele faz com que o codigo dentro da sua condição funcione
+
+if ($variavel1 == $variavel2 ){ //if (Condição)
+    echo "Variaveis são iguais!"; // codigo que roda quando o if for verdadeiro
+}
+else{ // caso o if e o else if forem falsos, o else é executado, sem nenhuma condição prescrita. 
+    echo "Falso!"; // resultado.
+}
+?>
