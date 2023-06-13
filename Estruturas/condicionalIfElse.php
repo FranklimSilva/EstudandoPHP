d2571d23eb8f3c6870c6f0f3532c015d913fc9b7
Franklim Porfirio <poorfiriof@gmail.com>
2023-06-13 19:44:58 -0300

    Condicional If else
    Como montamos o If e o Else, e a forma ternário, que nada mais é que o if resumido, contendo a mesma funcionalidade e lógica.

---
+++ Estruturas/condicionalIfElse.php
@@ -0,0 +1,16 @@
+<?php
+
+$nota = 7;
+
+//if normal
+// if($nota >= 7){
+//     echo "Aluno(a) aprovado!";
+// }
+// else{
+//     echo "Aluno(a) reprovado!";
+// }
+
+//if ternario. O ternario faz com que o codigo sejá resumido, contendo a mesma logica e funcionalidade.
+echo $nota >= 7 ? "Aluno(a) Aprovado" : "Aluno(a) Reprovado!";
+
+?>
